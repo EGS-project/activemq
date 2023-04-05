13199735655a95a839d4509a432b42533ccb8722
Miron <34632434+Sevelantis@users.noreply.github.com>
2023-04-05 13:49:48 +0100

    Create sender_example.py

---
+++ sender_example.py
@@ -0,0 +1,18 @@
+import stomp.connect as connect
+from src.message.utils import prepare_message
+from src.activemq.factory import ActiveMqConnectionFactory
+import src.config as config
+
+conn: connect.StompConnection11 = ActiveMqConnectionFactory.create_connection(
+    broker_host=config.ACTIVEMQ_HOST,
+    broker_port=config.ACTIVEMQ_PORT,
+    broker_username=config.ACTIVEMQ_USERNAME,
+    broker_password=config.ACTIVEMQ_PASSWORD,
+    listener=None
+)
+
+conn.send(
+    body=prepare_message(),
+    destination=config.ACTIVEMQ_CONVERT_IMAGE_QUEUE, 
+    headers={'my-custom-header': 'custom-header_dataaa'}
+    )
